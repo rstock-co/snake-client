df02877637f2d799b788baf156485b2c2e0f5f83
Richard Stock <rstock.co@gmail.com>
2022-08-31 21:12:56 +0000

    setup stdin to exit program via Ctrl-C

--- client.js
+++ client.js
@@ -1,30 +1,43 @@
 const net = require("net");
 
 const port = 50541;
-const host = '10.0.2.15'
-const clientName = 'RJS';
+const host = "10.0.2.15";
+const clientName = "RJS";
 
-const m = {
-  up: 'Move: up',
-  down: 'Move: down',
-  left: 'Move: left',
-  right: 'Move: right',
-}
+const move = {
+  up: "Move: up",
+  down: "Move: down",
+  left: "Move: left",
+  right: "Move: right",
+};
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (data) => {
+    if (data === "\u0003") {
+      process.exit();
+    }
+  });
+  return stdin;
+};
 
+setupInput();
 
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
     host,
-    port
+    port,
   });
 
-  const moveSnakeInterval = (direction, delay) => {
+  const moveSnakeOnInterval = (direction, delay) => {
     setInterval(() => {
-      conn.write(`${m[direction]}`)
+      conn.write(`${move[direction]}`);
     }, delay);
-  }
+  };
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -34,7 +47,7 @@ const connect = () => {
 
     // set client's name on server
     conn.write(`Name: ${clientName}`);
-    moveSnakeInterval('up',500)
+    moveSnakeOnInterval("up", 500);
   });
 
   conn.on("data", (message) => {
@@ -46,4 +59,4 @@ const connect = () => {
 
 module.exports = {
   connect,
-}
+};
