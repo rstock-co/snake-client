b48594095fcb8bfb426007710d23b162c21f69e0
Richard Stock <rstock.co@gmail.com>
2022-08-31 20:31:39 +0000

    moved connect function to client.js

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const net = require("net");
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (message) => {
-    console.log(`Message: ${message}`);
-  });
-
-  return conn;
-};
-
-console.log('connecting...')
+console.log('connecting...');
 connect();
