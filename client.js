2cd783092040ae912a56134c8bd020ba57ec2fac
Richard Stock <rstock.co@gmail.com>
2022-08-31 20:42:06 +0000

    send snake initials to server upon client connection

--- client.js
+++ client.js
@@ -1,14 +1,24 @@
 const net = require("net");
 
+const port = 50541;
+const clientName = 'RJS';
+
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
     host: '10.0.2.15',
-    port: 50541
+    port
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log(`Successfully connected to game server`);
+
+    // set client's name on server
+    conn.write(`Name: ${clientName}`);
+  });
 
   conn.on("data", (message) => {
     console.log(`Message: ${message}`);
