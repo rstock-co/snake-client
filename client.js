fa1eda3606044cbb11528cf3527bd7833ed61ab7
Richard Stock <rstock.co@gmail.com>
2022-08-31 23:07:00 +0000

    add wasd keystroke messages from client to server

--- client.js
+++ client.js
@@ -4,25 +4,12 @@ const port = 50541;
 const host = "10.0.2.15";
 const clientName = "RJS";
 
-const move = {
-  up: "Move: up",
-  down: "Move: down",
-  left: "Move: left",
-  right: "Move: right",
-};
-
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
     host,
     port,
   });
-
-  const moveSnakeOnInterval = (direction, delay) => {
-    setInterval(() => {
-      conn.write(`${move[direction]}`);
-    }, delay);
-  };
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -32,7 +19,6 @@ const connect = () => {
 
     // set client's name on server
     conn.write(`Name: ${clientName}`);
-    moveSnakeOnInterval("up", 500);
   });
 
   conn.on("data", (message) => {
--- input.js
+++ input.js
@@ -1,10 +1,36 @@
+// Stores the active TCP connection object.
+let connection;
+// console.log('upon declaration: ',connection)
+
+const move = {
+  up: "Move: up",
+  down: "Move: down",
+  left: "Move: left",
+  right: "Move: right",
+};
+
 const handleUserInput = (data) => {
+  // console.log('in user input: ',connection)
   if (data === "\u0003") {
     process.exit();
   }
+  if (data === 'w') {
+    connection.write(`${move.up}`)
+  }
+  if (data === 'a') {
+    connection.write(`${move.left}`)
+  }
+  if (data === 's') {
+    connection.write(`${move.down}`)
+  }
+  if (data === 'd') {
+    connection.write(`${move.right}`)
+  }
 };
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
+  // console.log('after assignment: ',connection)
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
