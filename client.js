377a5ce02eb086387b6884d886b5bd37d1236039
Richard Stock <rstock.co@gmail.com>
2022-08-31 20:53:30 +0000

    create moveSnakeInterval function

--- client.js
+++ client.js
@@ -1,14 +1,30 @@
 const net = require("net");
 
 const port = 50541;
+const host = '10.0.2.15'
 const clientName = 'RJS';
+
+const m = {
+  up: 'Move: up',
+  down: 'Move: down',
+  left: 'Move: left',
+  right: 'Move: right',
+}
+
+
 
 // establishes a connection with the game server
 const connect = () => {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    host,
     port
   });
+
+  const moveSnakeInterval = (direction, delay) => {
+    setInterval(() => {
+      conn.write(`${m[direction]}`)
+    }, delay);
+  }
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -18,6 +34,7 @@ const connect = () => {
 
     // set client's name on server
     conn.write(`Name: ${clientName}`);
+    moveSnakeInterval('up',500)
   });
 
   conn.on("data", (message) => {
